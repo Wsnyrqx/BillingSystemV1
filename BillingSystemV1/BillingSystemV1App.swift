82823c91575880e8a9fd8df0676fbe0e6e4ba18b
Wsnyrqx <152856834+Wsnyrqx@users.noreply.github.com>
2025-07-03 22:48:24 +0200

    Initial Commit

---
+++ BillingSystemV1/BillingSystemV1App.swift
@@ -0,0 +1,17 @@
+//
+//  BillingSystemV1App.swift
+//  BillingSystemV1
+//
+//  Created by Julian Schreiber on 03.07.25.
+//
+
+import SwiftUI
+
+@main
+struct BillingSystemV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
